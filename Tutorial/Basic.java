fa04b393cda6d4324ba372762aed6b7d689899d7
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-22 04:30:42 +0600

    Escape sequence

--- Tutorial/Basic.java
+++ Tutorial/Basic.java
@@ -1,8 +1,7 @@
 package Tutorial;
-
 public class Basic {
    public static void main(String[] args) {
-      System.out.println("Mehedi Hasan Himel");
-      System.out.println("01969038472");
+      System.out.println("Mehedi Hasan Himel. \n01969038472 \nDiploma in Computer Technology.");
+      System.out.println(" \"Programming in Java\"");
    }
 }
