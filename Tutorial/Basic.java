79e00d2c8c9049923d16727a8400ba970c662c04
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-22 04:08:15 +0600

    basic java

---
+++ Tutorial/Basic.java
@@ -0,0 +1,8 @@
+package Tutorial;
+
+public class Basic {
+   public static void main(String[] args) {
+      System.out.println("Mehedi Hasan Himel");
+      System.out.println("01969038472");
+   }
+}
