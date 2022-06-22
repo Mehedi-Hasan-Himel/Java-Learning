efb081c8e14e87c6261bfb5a153d0269fc787c2c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-22 12:03:17 +0600

    data type

---
+++ DataType.java
@@ -0,0 +1,23 @@
+public class DataType {
+   public static void main(String[] args) {
+      boolean b = true;
+      char c = 'a';
+      short s = 32677;
+      int i = 126587;
+      float f  = 10.2f;
+      double d = 10.2;
+
+      System.out.println("b = " + b);
+
+      System.out.println("c = " + c);
+
+      System.out.println("s = " + s);
+
+      System.out.println("i = " + i);
+
+      System.out.println("f = " + f);
+
+      System.out.println("d = " + d);
+
+   }
+}
---
+++ Tutorial/comment.java
@@ -0,0 +1,7 @@
+package Tutorial;
+
+public class comment {
+   public static void main(String[] args) {
+      System.out.println("Mehedi Hasan Himel"); // desplaying my name
+   }
+}
