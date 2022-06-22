9f433bf65124de4fdfad363b90683c72c6b8a3f7
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-22 12:15:14 +0600

    Format specifier

--- DataType.java
+++ DataType.java
@@ -4,20 +4,40 @@ public class DataType {
       char c = 'a';
       short s = 32677;
       int i = 126587;
-      float f  = 10.2f;
+      float f = 10.2f;
       double d = 10.2;
 
-      System.out.println("b = " + b);
+      // println() Start
 
-      System.out.println("c = " + c);
+      // System.out.println("b = " + b);
 
-      System.out.println("s = " + s);
+      // System.out.println("c = " + c);
 
-      System.out.println("i = " + i);
+      // System.out.println("s = " + s);
 
-      System.out.println("f = " + f);
+      // System.out.println("i = " + i);
 
-      System.out.println("d = " + d);
+      // System.out.println("f = " + f);
+
+      // System.out.println("d = " + d);
+
+      // println() End
+
+      // printf() Start
+
+      System.out.printf(" boolean b = %b\n" , b);
+
+      System.out.printf(" character c = %c\n" , c);
+
+      System.out.printf(" Short c = %d\n" , s);
+
+      System.out.printf(" Integer i = %d\n" , i);
+
+      System.out.printf(" Float f = %.2f\n" , f);
+
+      System.out.printf(" Double d = %.3f\n" , d);
+
+      // printf() End
 
    }
 }
